abb4a4b1d4ab414554917d11c2089e16df86c2d9
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-03 13:03:33 +0000

    function that compares two arguements and returns a mesage when its true or false

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('👏🏽👏🏽👏🏽 Assertion Passed: ' + '[' + actual + '] === ' + '[' + expected + ']');
+  } else
+    console.log('❌❌❌ Assertion Failed: ' + '[' + actual + '] !== ' + '[' + expected + ']');
+};
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1,1);
+assertEqual('Biology', 'Biology');
+assertEqual(10, 25);
