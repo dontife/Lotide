d4b01da42e7c18e21a9e8641adab3ebf4e5f4ffe
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-04 22:02:25 +0000

    This compares two arrays ands returns true or false based on the outcome

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👏🏽👏🏽👏🏽 Assertion Passed: [${actual}] === [${expected}]`);
+  } else
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+
+
+const eqArrays = function (someArray, anotherArray) {
+ for (let i =0; i < someArray.length; i++){
+   if (someArray[i] !== anotherArray[i]){
+     return false;
+   }
+ } 
+return true;
+}
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), true);
