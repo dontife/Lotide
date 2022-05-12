d2a81c2c4714ec94343bfd49f9d61ad84dfee830
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-12 18:09:59 +0000

    refactoring the code, using the require command to call the module in another file

--- head.js
+++ head.js
@@ -1,19 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👏🏽👏🏽👏🏽 Assertion Passed: [${actual}] === [${expected}]`);
-  } else
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-};
-
-/*assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1,1);
-assertEqual('Biology', 'Biology');
-assertEqual(10, 25);
-*/
+const assertEqual = require('./assertEqual');
 
 const head = function (someArray){
   let firstElement;
-  for (let i =0; i < someArray.length; i++){
+  for (let i = 0; i < someArray.length; i++){
     if(i === 0){
       firstElement = someArray[i];
       return firstElement;
@@ -21,7 +10,4 @@ const head = function (someArray){
   }
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
-assertEqual(head([1]), 1);
+module.exports = head;
