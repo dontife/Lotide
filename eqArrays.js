440f661605c271965827ca7622a21204e87beebf
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-12 18:59:52 +0000

    refactoring the code to allow module sharing

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,18 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👏🏽👏🏽👏🏽 Assertion Passed: [${actual}] === [${expected}]`);
-  } else
-    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-};
-
-
-
+/* This code compares two arrays content and returns true or false 
+// depending on the calculation.
+*/
 const eqArrays = function (someArray, anotherArray) {
   for (let i =0; i < someArray.length; i++){
+/* if the content of each individual array does not match with eaach other 
+   or the length of the both arrays are not the same return false, otherwise
+  return true
+*/    
     if ((someArray[i] !== anotherArray[i]) || (someArray.length !== anotherArray.length)){
       return false;
     }
   } 
  return true;
  };
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), true);
+// exporting our eqArrays function for use in another module
+module.exports = eqArrays;
