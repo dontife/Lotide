b572ed03267e5b94e6099815c8ea373e77632710
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-12 21:28:02 +0000

    using the chai and mocha package to test our tail file

--- test/tailTest.js
+++ test/tailTest.js
@@ -26,10 +26,3 @@ describe('#Tail', () => {
 
 
 
-/* Test Case: Check the original array
-const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(words[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(words[1], "Labs"); // ensure second element is "Labs"
-
-*/
