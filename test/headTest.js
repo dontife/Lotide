439465ef2305c353c2b1879094889f5822bd6fda
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-12 20:52:43 +0000

    using the mocha and chai package to test our file

--- test/headTest.js
+++ test/headTest.js
@@ -22,15 +22,3 @@ describe('#head' , () => {
 
 
 
-
-
-
-/*const assertEqual = require('../assertEqual');
-const head = require('../head');
-
- assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
-assertEqual(head([1]), 1);
-*/
-
