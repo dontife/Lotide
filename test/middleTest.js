ba00ec73dba85fba0e5a5ec150f5d3bcce6ea14d
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-12 22:11:02 +0000

    using the chai and mocha package to test the middle file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,28 @@
+const assert = require('chai').assert
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+
+describe('#Middle', () => {
+  it('should return undefined if the the argument passed to the function is not an array', () =>{
+    assert.isUndefined(middle('woman'), undefined);
+  });
+  it('should return [] if the length of the array is less than or equal to 2', () =>{
+    assert.deepEqual(middle([1,5]), []);
+  });
+  it('should return [2] - middle element, if the array length is an odd number [1,2,3]', () =>{
+    assert.deepEqual(middle([1,7,9]), [7]);
+  });
+  it('should return [2, 3] - middle element, if the array length is an even number [1,2,3,4]', () =>{
+    assert.deepEqual(middle([1,7,9,10]), [7,9]);
+  });
+  it('the array passed an argument to the function should remain unchanged [5,7,8] = [5,7,8]', () => {
+    let someArray = [7, 8, 9];
+    middle(someArray);
+    assert.deepEqual(someArray, [7, 8, 9]);
+  });
+  
+
+});
 
 
-// Test cases to see if the logic is sound
-console.log(middle([1]));  // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3]));// => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
-
-// Make sure the original array was not altered by the without function
 
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
