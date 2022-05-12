dc8b4677860c1169a3814d6943e9b6036d2addbf
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-12 19:58:18 +0000

    refactoring code for code sharing

--- middle.js
+++ middle.js
@@ -1,27 +1,7 @@
-/* This function comapres the content of two arrays
- and returns either true or false depending on the outcome
- */
-const eqArrays = function (someArray, anotherArray) {
-  for (let i =0; i < someArray.length; i++){
-    if ((someArray[i] !== anotherArray[i]) || (someArray.length !== anotherArray.length)){
-      return false;
-    }
-  } 
-  return true;
- };
-/*This function returns a message to confirm if the the assertion 
-inputted as an arguements passed or failed
-*/
- const assertArraysEqual = function(actual, expected) {
-  let result = eqArrays(actual, expected);
-    if (result === true) {
-      console.log(`👏🏽👏🏽👏🏽 Assertion Passed: [${actual}] === [${expected}]`);
-    } else
-      console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  };
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-/* This functions returns the middle element of an array
-*/
+
 const middle = function (someArray){
 // Create a new array to store the returned element
   let newArr = [];
@@ -40,14 +20,6 @@ const middle = function (someArray){
 };
 
 
-// Test cases to see if the logic is sound
-console.log(middle([1]));  // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3]));// => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
-// Make sure the original array was not altered by the without function
-const test = [4,5,7,0,1];
-middle(test);
-assertArraysEqual(test, [4,5,7,0,1]);
+
+
+module.exports = middle;
